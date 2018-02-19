65774ff82d9c9563875047ff0b3346846d7a8692
jimdrix <jimdrix@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-19 12:16:06 +0000

    Añadida algunas querys a los repositorios. Cambiada las "addres" del populate.

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@Query("select avg(r.trips.size),min(r.trips.size)," + "max(r.trips.size),sqrt(sum(r.trips.size *r.trips.size) / count(r.trips.size) - (avg(r.trips.size) * avg(r.trips.size))) from Ranger r")
 	String userStats();
+
+	@Query("select user from Reservation r join r.user user where r.rendezvous.id = ?1")
+	Collection<User> findAttendantsByRendezvous(int rendezvousId);
+
+	@Query("select user from Rendezvous r join r.user user where r.id = ?1")
+	User findByRendezvous(int rendezvousId);
 }
