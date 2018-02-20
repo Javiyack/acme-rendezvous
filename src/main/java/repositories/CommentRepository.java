06c1667cea89ace84f7c48e33c550f48fff65b7f
angonfer <angonfer@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-20 06:41:45 +0000

    Algunos convertidores, repositorios y servicios del proyecto anterior

---
+++ src/main/java/repositories/CommentRepository.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+package repositories;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import domain.Actor;
+import domain.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+	@Query("select c from Comment c where c.writer.id = ?1")
+	Collection<Comment> findAllByUser(int userId);
+}
