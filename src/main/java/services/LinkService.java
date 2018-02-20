254672352e59313b4444636c8f93af0bef91c3ae
angonfer <angonfer@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-20 06:59:24 +0000

    Resto de converters, de repositorios y de servicios (plantillas). Converters.xml

---
+++ src/main/java/services/LinkService.java
@@ -0,0 +1,74 @@
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Link;
+import repositories.LinkRepository;
+
+@Service
+@Transactional
+public class LinkService {
+
+	// Managed repositories ------------------------------------------------
+	@Autowired
+	private LinkRepository linkRepository;
+
+	// Supporting services 
+
+
+	// Constructor ----------------------------------------------------------
+	public LinkService() {
+		super();
+	}
+
+	// Methods CRUD ---------------------------------------------------------
+	public Link create() {
+
+		final Link result;
+
+		result = new Link();
+
+		return result;
+	}
+
+	public Collection<Link> findAll() {
+
+		Collection<Link> result;
+
+		result = this.linkRepository.findAll();
+		Assert.notNull(result);
+
+		return result;
+	}
+
+
+	public Link findOne(final int linkId) {
+		Link result;
+
+		result = this.linkRepository.findOne(linkId);
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public Link save(final Link link) {
+		Assert.notNull(link);
+		Link saved;
+
+		saved = this.linkRepository.save(link);
+
+		return saved;
+	}
+
+	public void delete(final Link link) {
+		Assert.notNull(link);
+
+		this.linkRepository.delete(link);
+	}
+
+}
