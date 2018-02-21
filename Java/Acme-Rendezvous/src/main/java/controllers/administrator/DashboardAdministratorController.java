b8dfce4e39df9ea0e2a3afdc5e79750b98ee790c
angonfer <angonfer@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-21 19:58:51 +0000

    Dashboard del administrador hasta el nivel A

--- Java/Acme-Rendezvous/src/main/java/controllers/administrator/DashboardAdministratorController.java
+++ Java/Acme-Rendezvous/src/main/java/controllers/administrator/DashboardAdministratorController.java
@@ -1,15 +1,20 @@
 
 package controllers.administrator;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import services.AdministratorService;
+import utilities.Statistics;
 import controllers.AbstractController;
 
 @Controller
-@RequestMapping("/dashboard/administrator")
+@RequestMapping("/dashboard")
 public class DashboardAdministratorController extends AbstractController {
 
 	@Autowired
@@ -22,47 +27,56 @@ public class DashboardAdministratorController extends AbstractController {
 		super();
 	}
 
-	//	@RequestMapping(value = "/display", method = RequestMethod.GET)
-	//	public ModelAndView display() {
-	//		ModelAndView result;
-	//
-	//		result = new ModelAndView("dashboard/display");
-	//
-	//		final Object statistics1 = this.administratorService.dashboardRendezvousesByUser();
-	//		result.addObject("dashboard.rendezvouses.by.user", statistics1);
-	//
-	//		final Object statistics2 = this.administratorService.dashboardRendezvousesRatioCreation();
-	//		result.addObject("dashboard.rendezvouses.ratio.creation", statistics2);
-	//
-	//		final Object statistics3 = this.administratorService.dashboardUsersPerRendezvous();
-	//		result.addObject("dashboard.users.per.rendezvous", statistics3);
-	//
-	//		final Object statistics4 = this.administratorService.dashboardRendezvousesRsvp();
-	//		result.addObject("dashboard.rendezvouses.rsvp", statistics4);
-	//
-	//		final Collection<Object> statistics5 = this.administratorService.dashboardRendezvousesTop10();
-	//		result.addObject("dashboard.rendezvouses.top10", statistics5);
-	//
-	//		final Object statistics6 = this.administratorService.dashboardAnnouncementsRatio();
-	//		result.addObject("dashboard.announcements.ratio", statistics6);
-	//
-	//		final Collection<Object> statistics7 = this.administratorService.dashboardAnnouncementsAbove75();
-	//		result.addObject("dashboard.announcements.above.75", statistics7);
-	//
-	//		final Collection<Object> statistics8 = this.administratorService.dashboardRendezvousesLinked();
-	//		result.addObject("dashboard.rendezvouses.linked", statistics8);
-	//
-	//		final Object statistics9 = this.administratorService.dashboardQuestionsPerRendezvous();
-	//		result.addObject("dashboard.questions.rendezvous", statistics9);
-	//
-	//		final Object statistics10 = this.administratorService.dashboardAnswersPerRendezvous();
-	//		result.addObject("dashboard.answers.rendezvous", statistics10);
-	//
-	//		final Object statistics11 = this.administratorService.dashboardRepliesPerComment();
-	//		result.addObject("dashboard.replies.comment", statistics11);
-	//
-	//		return result;
+		@RequestMapping(value = "/list", method = RequestMethod.GET)
+		public ModelAndView display() {
+			ModelAndView result;
+			Statistics s;
+	
+			result = new ModelAndView("dashboard/display");
+	
+			final Object[] stats1 = this.administratorService.dashboardRendezvousesByUser();
+			s = new Statistics(stats1);
+			result.addObject("dashboard1", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Object[] stats2 = this.administratorService.dashboardRendezvousesRatioCreation();
+			s = new Statistics(stats2);
+			result.addObject("dashboard2", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Object[] stats3 = this.administratorService.dashboardUsersPerRendezvous();
+			s = new Statistics(stats3);
+			result.addObject("dashboard3", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Object[] stats4 = this.administratorService.dashboardRendezvousesRsvp();
+			s = new Statistics(stats4);
+			result.addObject("dashboard4", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Collection<Object> stats5 = this.administratorService.dashboardRendezvousesTop10();
+			result.addObject("dashboard5", stats5);
+	
+			final Object[] stats6 = this.administratorService.dashboardAnnouncementsRatio();
+			s = new Statistics(stats6);
+			result.addObject("dashboard6", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Collection<Object> statistics7 = this.administratorService.dashboardAnnouncementsAbove75();
+			result.addObject("dashboard7", statistics7);
+	
+			final Collection<Object> statistics8 = this.administratorService.dashboardRendezvousesLinked();
+			result.addObject("dashboard8", statistics8);
+	
+			final Object[] stats9 = this.administratorService.dashboardQuestionsPerRendezvous();
+			s = new Statistics(stats9);
+			result.addObject("dashboard9", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Object[] stats10 = this.administratorService.dashboardAnswersPerRendezvous();
+			s = new Statistics(stats10);
+			result.addObject("dashboard10", new double[]{s.getMean(), s.getStdDev()} );
+	
+			final Object[] stats11 = this.administratorService.dashboardRepliesPerComment();
+			s = new Statistics(stats11);
+			result.addObject("dashboard11", new double[]{s.getMean(), s.getStdDev()} );
+	
+			return result;
 
-	//}
+	}
 
 }
