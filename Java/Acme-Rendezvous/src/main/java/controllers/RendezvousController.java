bb6667220c38804da9a48b92e0479354b491241a
jimdrix <jimdrix@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-22 14:11:50 +0000

    Rendezvouses similares

--- Java/Acme-Rendezvous/src/main/java/controllers/RendezvousController.java
+++ Java/Acme-Rendezvous/src/main/java/controllers/RendezvousController.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import services.RendezvousService;
 import domain.Rendezvous;
@@ -39,6 +39,21 @@ public class RendezvousController {
 		result = new ModelAndView("rendezvous/list");
 		result.addObject("rendezvouses", rendezvouses);
 		result.addObject("requestUri", "rendezvous/list.do");
+
+		return result;
+	}
+
+	//List Similar---------------------------------------------------------------		
+	@RequestMapping(value = "/listSimilar", method = RequestMethod.GET)
+	public ModelAndView listSimilar(@RequestParam final int rendezvousId) {
+
+		ModelAndView result;
+
+		final Collection<Rendezvous> rendezvouses = this.rendezvousService.findSimilarRendezvous(rendezvousId);
+
+		result = new ModelAndView("rendezvous/listSimilar");
+		result.addObject("rendezvouses", rendezvouses);
+		result.addObject("requestUri", "rendezvous/listSimilar.do");
 
 		return result;
 	}
