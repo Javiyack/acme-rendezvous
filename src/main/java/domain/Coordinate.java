5c34980c332c3f82c33c4e5595cac49f4e414bf2
jimdrix <jimdrix@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-26 15:53:38 +0000

    Corregido Cancelado de mis reservas.

--- src/main/java/domain/Coordinate.java
+++ src/main/java/domain/Coordinate.java
@@ -4,7 +4,6 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Embeddable
@@ -17,30 +16,30 @@ public class Coordinate {
 
 	@Pattern(regexp = "^([0-9]*\\.?[0-9])|([0-9]*\\.?)$")
 	public String getLongitude() {
-		String res = (longitude==null)?"":longitude.toString();
+		final String res = (this.longitude == null) ? "" : this.longitude.toString();
 		return res;
 	}
 	public void setLongitude(final String longitude) {
-		Double lon=null;
+		Double lon = null;
 		try {
-			Object res = Double.parseDouble(longitude);
-			lon = (res instanceof Double)?(Double)res:null;
-		} catch (NumberFormatException e) {
+			final Object res = Double.parseDouble(longitude);
+			lon = (res instanceof Double) ? (Double) res : null;
+		} catch (final NumberFormatException e) {
 		}
 		this.longitude = lon;
 	}
 
 	@Pattern(regexp = "^([0-9]*\\.?[0-9])|([0-9]*\\.?)$")
 	public String getLatitude() {
-		String res = (latitude==null)?"":latitude.toString();
+		final String res = (this.latitude == null) ? "" : this.latitude.toString();
 		return res;
 	}
 	public void setLatitude(final String latitude) {
-		Double lat=null;
+		Double lat = null;
 		try {
-			Object res = Double.valueOf(latitude);
-			lat = (res instanceof Double)?(Double)res:null;
-		} catch (NumberFormatException e) {
+			final Object res = Double.valueOf(latitude);
+			lat = (res instanceof Double) ? (Double) res : null;
+		} catch (final NumberFormatException e) {
 		}
 		this.latitude = lat;
 	}
