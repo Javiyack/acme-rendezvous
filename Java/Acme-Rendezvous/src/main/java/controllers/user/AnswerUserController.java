67d8201d681fe39990c594aed687c1089bc82d21
frallafer <frallafer@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-27 14:24:38 +0000

    2. Answer the questions that are associated with a rendezvous that he or she’s RSVP-ing now.

--- Java/Acme-Rendezvous/src/main/java/controllers/user/AnswerUserController.java
+++ Java/Acme-Rendezvous/src/main/java/controllers/user/AnswerUserController.java
@@ -10,19 +10,31 @@
 
 package controllers.user;
 
+import java.util.Collection;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import services.AnswerService;
 import controllers.AbstractController;
 import domain.Answer;
+import domain.Question;
+import domain.Reservation;
+import domain.User;
+import forms.FormularioPreguntas;
+import services.AnswerService;
+import services.QuestionService;
+import services.RendezvousService;
+import services.ReservationService;
+import services.UserService;
 
 @Controller
 @RequestMapping("/answer/user")
@@ -31,9 +43,26 @@ public class AnswerUserController extends AbstractController {
 	// Services ---------------------------------------------------------------
 
 	@Autowired
-	private AnswerService	answerService;
+	private AnswerService 			answerService;
+	@Autowired
+	private QuestionService 		questionService;
+	@Autowired
+	private ReservationService		reservationService;
+	@Autowired
+	private UserService 			userService;
+	@Autowired
+	private RendezvousService 		rendezvousService;
 
+	
+	// Fail page
+	public ModelAndView fail() {
+		ModelAndView result;
+		result = new ModelAndView("/answer/fail");
 
+		return result;
+	}
+
+	
 	// Creation ---------------------------------------------------------------
 
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
@@ -44,7 +73,6 @@ public class AnswerUserController extends AbstractController {
 		answer = this.answerService.create(questionId, rendezvousId);
 		result = this.createEditModelAndView(answer);
 
-		
 		return result;
 	}
 
@@ -59,15 +87,71 @@ public class AnswerUserController extends AbstractController {
 		else
 			try {
 				this.answerService.save(answer);
-				result = new ModelAndView("redirect:/rendezvous/user/reserve.do?rendezvousId="+answer.getReservation().getRendezvous().getId());
-				
+				result = new ModelAndView("redirect:/rendezvous/user/reserve.do?rendezvousId="
+						+ answer.getReservation().getRendezvous().getId());
+
 			} catch (final Throwable oops) {
 				oops.printStackTrace();
 				result = this.createEditModelAndView(answer, "answer.commit.error");
 			}
-		
 
 		return result;
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute("formularioEjemplo") final FormularioPreguntas formulario,
+			final BindingResult binding) {
+		final User user = this.userService.findByPrincipal();
+		Assert.notNull(user);
+		Assert.isTrue(user.equals(formulario.getUser()));
+		Boolean todasRespondidas = true;
+		// Comprobamos que las preguntas estan contestadas si no es cadena vacia o solo
+		// espacios
+		// Para ello:
+		// 1º recuperamos las Questions
+		Collection<Question> preguntas = questionService.findAllByRendezvousId(formulario.getRendezvous().getId());
+		for (Question question : preguntas) {
+			if (formulario.getCuestionario().get(question.getText()).trim().length() == 0) {
+				todasRespondidas = false;
+			}
+		}
+
+		// Creamos y guardamos la reserva
+		if (todasRespondidas) {
+			if (!user.getAdult() && formulario.getRendezvous().getAdult())
+				return new ModelAndView("redirect:/");
+
+			Reservation reservation = this.reservationService.findReservationByUserAndRendezvous(user, formulario.getRendezvous());
+
+			if (reservation == null) {
+				reservation = this.reservationService.create();
+				Assert.notNull(reservation);
+
+				Reservation done;
+				done = this.rendezvousService.reserveRendezvous(reservation, formulario.getRendezvous());
+				Assert.notNull(done);
+
+				reservation = this.reservationService.save(done);
+
+			} else if (reservation.isCanceled()) {
+				reservation.setCanceled(false);
+				reservation = this.reservationService.save(reservation);
+			}	
+			
+			
+			for (Question question : preguntas) {
+				Answer respuesta = answerService.create(question.getId(), formulario.getRendezvous().getId());
+				respuesta.setText(formulario.getCuestionario().get(question.getText()).trim());
+				answerService.save(respuesta);
+			}
+
+		} else {
+			return new ModelAndView("redirect:/answer/user/fail.do");
+		}
+		// Guardamos las respuestas
+
+		return new ModelAndView("redirect:/");
+
 	}
 
 	// Ancillary methods ------------------------------------------------------
