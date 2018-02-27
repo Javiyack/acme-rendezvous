4230b41e8ac97f4e36a0a6c436f207de3541f72c
ferramvil <ferramvil@a61fbbbb-cd34-446d-9098-54a3d1470b43>
2018-02-27 17:43:41 +0000

    Añadido @Valid a las dependencias en las clases

--- src/main/java/controllers/RendezvousController.java
+++ src/main/java/controllers/RendezvousController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.RendezvousService;
+import services.UserService;
 import domain.Rendezvous;
 
 @Controller
